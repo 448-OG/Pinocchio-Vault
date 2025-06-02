ac90c49ef8a0c0cb06f076102d20a77a71372723
448 OG <superuser@448.africa>
2025-06-02 19:10:47 +0300

    Finish the deposit challange
    - The payer can deposit SOL in the vault
    - Use LiteSVM to test the validity of these claims

---
+++ src/instructions.rs
@@ -0,0 +1,24 @@
+use pinocchio::program_error::ProgramError;
+
+pub struct DepositInstructionData {
+    pub amount: u64,
+}
+
+impl<'a> TryFrom<&'a [u8]> for DepositInstructionData {
+    type Error = ProgramError;
+
+    fn try_from(data: &'a [u8]) -> Result<Self, Self::Error> {
+        if data.len() != size_of::<u64>() {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+        let amount = u64::from_le_bytes(data.try_into().unwrap());
+
+        // Instruction Checks
+        if amount.eq(&0) {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+        Ok(Self { amount })
+    }
+}
---
+++ tests/deposit.rs
@@ -0,0 +1,63 @@
+use blueshift_vault::{Deposit, ID as PROGRAM_ID, SEED};
+use litesvm::LiteSVM;
+use solana_instruction::{AccountMeta, Instruction};
+use solana_keypair::Keypair;
+use solana_pubkey::Pubkey;
+use solana_signer::Signer;
+use solana_transaction::Transaction;
+
+const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
+
+#[test]
+fn deposit_to_vault() {
+    let mut svm = LiteSVM::new();
+
+    let payer = Keypair::new();
+    svm.airdrop(&payer.pubkey(), 2 * LAMPORTS_PER_SOL).unwrap();
+
+    let program_bytes = include_bytes!("../target/deploy/blueshift_vault.so");
+    let program_id = Pubkey::new_from_array(PROGRAM_ID);
+
+    let (vault_pubkey, bump) = solana_pubkey::Pubkey::try_find_program_address(
+        &[SEED, payer.pubkey().as_array()],
+        &program_id,
+    )
+    .unwrap();
+
+    let balance = svm.get_balance(&payer.pubkey()).unwrap();
+    assert_eq!(balance, 2 * LAMPORTS_PER_SOL);
+
+    svm.add_program(program_id, program_bytes);
+
+    let mut data = Vec::<u8>::default();
+    data.push(*Deposit::DISCRIMINATOR);
+    data.push(bump);
+    data.extend_from_slice(&LAMPORTS_PER_SOL.to_le_bytes());
+
+    let ix = Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(payer.pubkey(), true),
+            AccountMeta::new(vault_pubkey, false),
+            AccountMeta::new(solana_system_interface::program::ID, false),
+        ],
+        data,
+    };
+
+    let recent_blockhash = svm.latest_blockhash();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[ix],
+        Some(&payer.pubkey()),
+        &[&payer],
+        recent_blockhash,
+    );
+
+    // Simulate tx
+    let sim_res = svm.simulate_transaction(tx.clone()).unwrap();
+    let meta = svm.send_transaction(tx).unwrap();
+    assert_eq!(sim_res.meta, meta);
+
+    let deposit_balance = svm.get_balance(&vault_pubkey).unwrap_or_default();
+    assert_eq!(LAMPORTS_PER_SOL, deposit_balance);
+}
